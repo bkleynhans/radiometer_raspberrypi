f240a09fdc9c1dbe7d4a4473656c350b71ad09ac
Benjamin Kleynhans <kleynhansbenjamin@gmail.com>
2020-08-25 17:44:39 +0000

    Add functionality to upload files to remote server, including creation of directories on remote server

--- src/filemanager.py
+++ src/filemanager.py
@@ -15,6 +15,7 @@
 
 # Imports
 import os, subprocess
+import paramiko
 import json
 import time
 import pdb
@@ -50,6 +51,85 @@ class Filemanager():
         filehandle.writelines(data_string)
         filehandle.close()
         
-    def upload_to_server(self, filename, protocol):
+    
+    def connect_sftp(self):
         
-        pass
+        self.ssh_client = paramiko.SSHClient()
+        self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        
+        connected = False
+        
+        for host in self.preferences['protocol']['ssh']['servers']:
+            if not connected:
+                try:
+                    self.ssh_client.connect(
+                        hostname=host,
+                        username=self.preferences['protocol']['ssh']['username'],
+                        password=self.preferences['protocol']['ssh']['password']
+                    )
+                    
+                    connected = true
+                except:
+                    continue
+                    
+    
+    def build_structure(self, site_name, filename):
+        
+        # Extract the year and month from the filename
+        year = filename[:4]
+        month = filename[4:6]
+        
+        # Construct the path to get a directory listing for existing sites
+        path_to_sites = self.preferences['protocol']['ssh']['destinationPath']
+        sites = self.get_remote_contents(path_to_sites)
+        path_to_years = os.path.join(path_to_sites, site_name)
+        
+        # If the site name does not exist, create it
+        if not site_name in sites:            
+            stdin, stdout, stderr = self.ssh_client.exec_command("mkdir {}".format(path_to_years))
+        
+        # Construct the path to get a directory listing for existing years
+        years = self.get_remote_contents(path_to_years)
+        path_to_months = os.path.join(path_to_years, str(year))
+        
+        # If the year does not exist, create it
+        if not year in years:
+            stdin, stdout, stderr = self.ssh_client.exec_command("mkdir {}".format(path_to_months))
+        
+        # Construct the path to get a directory listing for existing months
+        months = self.get_remote_contents(path_to_months)
+        path_to_days = os.path.join(path_to_months, str(month))
+        
+        # If the month does not exist, create it
+        if not month in months:            
+            stdin, stdout, stderr = self.ssh_client.exec_command("mkdir {}".format(path_to_days))
+            
+                                
+    def get_remote_contents(self, path):
+        
+        stdin, stdout, stderr = self.ssh_client.exec_command("ls {}".format(path))
+        
+        unformatted_entries = stdout.readlines()
+        formatted_entries = []
+        
+        for entry in unformatted_entries:
+            formatted_entries.append(entry.rstrip('\n'))
+            
+        return formatted_entries
+        
+        
+    def get_local_contents(self, path):
+        
+        return [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    
+    
+    def upload_to_server(self, full_source_path, full_destination_path):
+        
+        sftp_client = self.ssh_client.open_sftp()
+        sftp_client.put(full_source_path, full_destination_path)
+        sftp_Client.close()
+
+
+    def move_to_uploaded(self, full_source_path):
+        
+        pdb.set_trace()
--- src/radiometer.py
+++ src/radiometer.py
@@ -41,6 +41,9 @@ class Radiometer:
         # Should data be uploaded
         self.upload_data = False
         
+        # Save today's date to check for 24-hour intervals
+        self.today = datetime.now(timezone.utc).strftime('%Y%m%d')
+        
         # Create instance of filemanager class which does all file related actions
         self.filemanager = Filemanager(args)
 
@@ -56,16 +59,22 @@ class Radiometer:
                             self.args['preferences']['provider']
                         )
         
-        # ~ while True:
-            # ~ self.program_loop()
-        self.program_loop()
+        while True:
+            self.program_loop()
         
     
     def program_loop(self):
         
+        if datetime.now(timezone.utc).strftime('%Y%m%d') != self.today:
+            self.upload_data = True
+            self.today = datetime.now(timezone.utc).strftime('%Y%m%d')
+        
         # Check if this is the initial boot of the device, and set some values
         if self.initial_startup:
             self.startup_procedure()
+            
+        self.initial_startup = True
+        self.upload_data = True
         
     
     def startup_procedure(self):
@@ -75,7 +84,8 @@ class Radiometer:
         
         # If we need to upload a data file, upload it
         if self.upload_data:
-            pass
+            self.upload_to_server()
+            self.update_data = False
             
         # Update the current day from the internet
         self.set_clock()
@@ -90,6 +100,35 @@ class Radiometer:
         
         # Disconnect from internet and turn off modem
         self.sim7600.disconnect()
+
+
+    def upload_to_server(self):
+        
+        self.filemanager.connect_sftp()
+        
+        files_to_upload = self.filemanager.get_local_contents(self.args['preferences']['sourcePath'])
+        
+        self.filemanager.build_structure(self.args['preferences']['siteName'], self.args['filename'])
+        
+        for source_file in files_to_upload:            
+            full_source_path = os.path.join(
+                                    self.args['preferences']['sourcePath'],
+                                    source_file
+                                )
+                                
+            full_destination_path = os.path.join(
+                                    self.args['preferences']['protocol']['ssh']['destinationPath'],
+                                    self.args['preferences']['siteName'],
+                                    source_file[:4],
+                                    source_file[4:6],
+                                    source_file
+                                )
+
+            try:
+                self.filemanager.upload_to_server(full_source_path, full_destination_path)
+                self.filemanager.move-to_uploaded()
+            except:
+                continue
     
     
     def set_clock(self):
@@ -111,15 +150,11 @@ class Radiometer:
         
         print("    --- Building Filename ---")
         
-        # Clean the filename variable
-        self.args['filename'] = None
-        
         # Set the new filename
-        self.args['filename'] = self.args['preferences']['siteName']
-        self.args['filename'] += "_"
-        self.args['filename'] += str(datetime.now(timezone.utc).strftime('%y'))
+        self.args['filename'] = str(datetime.now(timezone.utc).strftime('%Y'))
         self.args['filename'] += str(datetime.now(timezone.utc).strftime('%m'))
         self.args['filename'] += str(datetime.now(timezone.utc).strftime('%d'))
+        self.args['filename'] += ".csv"
         
     
     def build_heading(self):
