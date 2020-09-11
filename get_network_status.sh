98282a7007339ca4a8a3b7c07c0a33233a7a7f8d
Benjamin Kleynhans <kleynhansbenjamin@gmail.com>
2020-09-11 13:15:43 +0000

    Updated to add a reset and timers for processes to complete

--- get_network_status.sh
+++ get_network_status.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# State changes can take a few moments to be executed, which
+# is why they have sleep timers
+
+# Reset the device to ensure it isn't in an unsupported state
+sudo qmicli -d /dev/cdc-wdm0 --dms-set-operating-mode='reset'
+sleep 20  # reset takes longer because it has to power cycle the radio array 
+
 # Set device online
 sudo qmicli -d /dev/cdc-wdm0 --dms-set-operating-mode='online'
+sleep 10
 
 # Get current provider information
 sudo qmicli -d /dev/cdc-wdm0 --nas-get-home-network
@@ -11,3 +19,4 @@ sudo qmicli -d /dev/cdc-wdm0 --nas-get-signal-strength
 
 # Set device offline
 sudo qmicli -d /dev/cdc-wdm0 --dms-set-operating-mode='offline'
+sleep 10
